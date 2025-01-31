694a0cb510ef367d3325655e04fe2de130f657ba
eyeris-vhs <vhhsaggese@gmail.com>
2025-01-31 15:42:37 -0500

    Readme and hwk0

---
+++ Homework 0/HomeworkZero.java
@@ -0,0 +1,7 @@
+public class HomeworkZero {
+    public static void main(String[] args)
+    //"thing that doesn't change and doesn't return a val", "main function that expects strings"
+    {
+    System.out.println("Hello, World.");
+    System.out.println("Legally, my name is Vincent Hugh Saggese, but I would ask that you refer to me as Iris.");
+}}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # COS225_Iris_S
 COS 225 homework repo 
+This repo holds all homework files for COS 225. Please see relevant folders.
